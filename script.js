80afc309267c5dad58713401ad7a0ef135b3c437
Richard Walter <richardwalter515@gmail.com>
2020-10-07 14:08:49 -0400

    created HTML layout and pseudocode for JS

---
+++ script.js
@@ -0,0 +1,11 @@
+//enter city name (val) in search bar
+
+//on click of search button
+
+//pull city info from API
+
+//consol log it, and save to local storage so user can see previous cities
+
+//display current city data in div
+
+//create 5 day forecast by prepending each day in five day forecast div
